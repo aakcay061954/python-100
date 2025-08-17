87940e95d96c425a238f0916a98f42bf8b6d40c3
Abdullah Akcay <abdullahakcay84@gmail.com>
2025-08-16 22:25:14 -0700

    add description ro brand name generator

--- projects/brand-name-generator/main.py
+++ projects/brand-name-generator/main.py
@@ -1,3 +1,4 @@
+#Asks for the city you grew up in and a pet name, then prints a band name.
 print("Welcome to the Band Name Generator.")
 city = input("Which city did you grow up in?\n")
 pet = input("What is the name of a pet?\n")
